e70b8d9690f08b5bb9d03fdaf91528b0e0867b19
Joseph Merigala <104745031+13HJoe@users.noreply.github.com>
2024-03-09 09:37:08 +0530

    Update cryptF.py
    Added checks to length of the key
    Updated fail-safe operations to work in order

--- cryptF.py
+++ cryptF.py
@@ -1,51 +1,50 @@
 import sys
 import os
+import glob
 import hashlib
 import maskpass
 from termcolor import colored
-
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
-
-if len(sys.argv) <= 1:
-    print("Invalid Usage")
-    print("Use <cryptF -h OR -help> for information")
-    exit(0)
-
-# ---------------------------------------------------------#
 # ---------------------------------------------------------#
 if sys.argv[1] == "-h" or sys.argv[1] == "-help":
-    """
-    with open('help.txt','r') as file:
-      help_data = file.read()
-      print(help_data)
-    """
     print("Syntax - 'cryptF -e unencrypted.txt  key'\n")
     print("       - 'cryptF -d encrypted.txt  key decryptedFile' \n")
     print("=> key : length = 16 [The length of the 16 must only be 16 characters]")
     exit(0)
+#---------------------------------------------------------#
+if len(sys.argv) <= 1:
+    print("Invalid Usage")
+    print("Use <cryptF -h OR -help> for information")
+    exit(0)
+#----------------------------------------------------------#
+flag = sys.argv[1]
+if flag != "-e" and flag != "-d":
+    print("Invalid Usage")
+    print("Use <cryptF -h OR -help> for information")
+    exit(0)
 # ---------------------------------------------------------#
-# ---------------------------------------------------------#
-
+filename = sys.argv[2]
+list_check_file = glob.glob(filename)
+if len(list_check_file) == 0:
+    cwd1 = os.getcwdb()
+    print(colored("File not found","light_red"),colored("in the directory ->","light_red"),colored(cwd1.decode('utf-8'),"light_red"))
+    exit(0)
+#----------------------------------------------------------#
 key = maskpass.askpass(prompt="Key:", mask="*")
-#keyString = sys.argv[3]  # Extract key from the command line
+if len(key) != 16:
+    print(colored("Invalid Key Length\n=> key : length = 16 [The length of the 16 must only be 16 characters]","light_red"))
+    exit(0)                                                                                                   
 key = key.encode("utf-8")  # Encode key to binary data
-#key_bytes = bytes(key,"utf-8")
-# ---------------------------------------------------------#
 # ---------------------------------------------------------#
 # Encryption Flag
-if sys.argv[1] == "-e":
-    filename = sys.argv[2]
+if flag == "-e":
     with open(filename, "rb") as file:  #'rb' opens file; -Read-Binary mode
         data = file.read()
     del_option = input(colored("Delete Unencrypted File? Y/N \n ->","light_red"))
     if del_option == "Y":
         os.remove(filename)  # remove sensitive unencrypted file
-    
-    # Hash Key and store to new text file
-    print(key)
-    hash = hashlib.sha256(key).hexdigest() #key is already encoded to utf-8
-    print(hash)
+    hash = hashlib.sha256(key).hexdigest() #key is already encoded to utf-8s     # Hash Key and store to new text file
     key_hash_filename = filename[:-4] + "keyHash.txt"
     with open(key_hash_filename,'w') as key_hash_file:
         key_hash_file.write(hash)
@@ -58,13 +57,9 @@ if sys.argv[1] == "-e":
     with open(newfile, "wb") as file:
         file.write(ciphertext)
     print(colored("Encrypted Data -> ","light_green"), newfile)
-
-# ---------------------------------------------------------#
 # ---------------------------------------------------------#
 # Decryption Flag
-elif sys.argv[1] == "-d":
-    filename = sys.argv[2]
-
+elif flag == "-d":
     key_hash_filename = filename[:-8]+"keyHash.txt"
     with open(key_hash_filename,'r') as key_hash_file:
         hash = key_hash_file.read().encode('utf-8')
@@ -90,9 +85,3 @@ elif sys.argv[1] == "-d":
         os.remove(filename)
         print(colored("File removed successfully","light_green"))
         print(colored("Be sure to remove padded data","light_yellow"))
-
-# ---------------------------------------------------------#
-else:
-    print("Invalid Usage")
-    print("Use <cryptF -h OR -help> for information")
-    exit(0)
